1237068982a5aa39f71aa00dfde995d92152e29f
Zabalkanskiy <azabalkanskij@hotmail.com>
2025-09-13 12:54:52 +0300

    fix gallery_page.dart

--- lib/feature/gallery/view/gallery_page.dart
+++ lib/feature/gallery/view/gallery_page.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
@@ -50,11 +52,19 @@ class GalleryPage extends StatelessWidget {
             //   scale: 0.5, // коэффициент масштабирования
             //   child: SvgPicture.asset("assets/icon/ic_login.svg"),
             // ),
-            leading: SvgPicture.asset(
-              "assets/icon/ic_login.svg",
-              width: 10,
-              height: 10,
-              fit: BoxFit.scaleDown, // или BoxFit.contain
+            leading: GestureDetector(
+              onTap: () async {
+                await FirebaseAuth.instance.signOut();
+                if (context.mounted) {
+                  Navigator.of(context).pushReplacementNamed('/auth');
+                }
+              },
+              child: SvgPicture.asset(
+                "assets/icon/ic_login.svg",
+                width: 10,
+                height: 10,
+                fit: BoxFit.scaleDown, // или BoxFit.contain
+              ),
             ),
             centerTitle: true,
             title: const Text('Галерея', style: TextStyle(color: Color(0xFFEEEEEE),),  textAlign: TextAlign.center, ),
@@ -65,49 +75,85 @@ class GalleryPage extends StatelessWidget {
                 right: Radius.circular(15),
               ),
             ),
-            actions: [
-              IconButton(
-                icon: const Icon(Icons.exit_to_app),
-                onPressed: () async {
-                  await FirebaseAuth.instance.signOut();
-                  if (context.mounted) {
-                    Navigator.of(context).pushReplacementNamed('/auth');
-                  }
-                },
-              ),
-            ],
           ),
-          floatingActionButton: FloatingActionButton(
-            onPressed: () => Navigator.of(context).pushNamed('/editor'),
-            child: const Icon(Icons.add),
-          ),
-          body: StreamBuilder<QuerySnapshot<Map<String, dynamic>>>(
-            stream: images,
-            builder: (context, snapshot) {
-              if (!snapshot.hasData)
-                return const Center(child: CircularProgressIndicator());
-              final docs = snapshot.data!.docs;
-              if (docs.isEmpty) return const Center(child: Text('Нет изображений'));
-              return GridView.builder(
-                padding: const EdgeInsets.all(8),
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 3,
-                  crossAxisSpacing: 4,
-                  mainAxisSpacing: 4,
+          // floatingActionButton: FloatingActionButton(
+          //   onPressed: () => Navigator.of(context).pushNamed('/editor'),
+          //   child: const Icon(Icons.add),
+          // ),
+          body: Column(
+            children: [
+              Expanded(
+                child: StreamBuilder<QuerySnapshot<Map<String, dynamic>>>(
+                  stream: images,
+                  builder: (context, snapshot) {
+                    if (snapshot.connectionState == ConnectionState.waiting) {
+                      return const Center(child: CircularProgressIndicator());
+                    }
+                    if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
+                      return const Center(child: Text('Нет изображений'));
+                    }
+                    final docs = snapshot.data!.docs;
+                    return GridView.builder(
+                      padding: const EdgeInsets.all(8),
+                      gridDelegate:
+                      const SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 3,
+                        crossAxisSpacing: 4,
+                        mainAxisSpacing: 4,
+                      ),
+                      itemCount: docs.length,
+                      itemBuilder: (context, index) {
+                        final data = docs[index].data();
+                        final base64String = data['base64'] as String?;
+                        if (base64String == null) {
+                          return const SizedBox.shrink();
+                        }
+                        try {
+                          final bytes = base64Decode(base64String);
+                          return GestureDetector(
+                            onTap: () => Navigator.of(context).pushNamed(
+                              '/editor',
+                              arguments: bytes,
+                            ),
+                            child: Image.memory(
+                              bytes,
+                              fit: BoxFit.cover,
+                            ),
+                          );
+                        } catch (e) {
+                          return const Icon(Icons.error);
+                        }
+                      },
+                    );
+                  },
                 ),
-                itemCount: docs.length,
-                itemBuilder: (context, index) {
-                  final data = docs[index].data();
-                  final url = data['url'] as String?;
-                  if (url == null) return const SizedBox.shrink();
-                  return GestureDetector(
-                    onTap: () =>
-                        Navigator.of(context).pushNamed('/editor', arguments: url),
-                    child: Image.network(url, fit: BoxFit.cover),
-                  );
-                },
-              );
-            },
+              ),
+              // Кнопка "Создать" внизу экрана
+              Container(
+                width: double.infinity,
+                height: 48,
+                margin: const EdgeInsets.all(16),
+                decoration: BoxDecoration(
+                  gradient: const LinearGradient(
+                    colors: [Color(0xFF8924E7), Color(0xFF6A46F9)],
+                    begin: Alignment.topLeft,
+                    end: Alignment.bottomRight,
+                  ),
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+                child: TextButton(
+                  onPressed: () => Navigator.of(context).pushNamed('/editor'),
+                  child: const Text(
+                    "Создать",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 40,)
+            ],
           ),
         ),
       ],
