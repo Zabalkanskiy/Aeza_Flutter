834dbad4b2bfed3b40e33287c799f915165fcdb6
Zabalkanskiy <azabalkanskij@hotmail.com>
2025-09-14 15:31:08 +0300

    debug banner false

--- lib/app.dart
+++ lib/app.dart
@@ -26,6 +26,7 @@ class AezaApp extends StatelessWidget {
       ],
       child: MaterialApp(
         title: 'AezaFlutter',
+        debugShowCheckedModeBanner: false,
         theme: ThemeData(
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
           useMaterial3: true,
